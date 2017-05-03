686311e6358bcff076f5ad4b64d9aca08d51bb3f
ZacharyRobertson <ztrsec@hotmail.com>
2017-05-03 10:36:37 +1000

    Add files via upload
    These are the assets used in the project

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private AudioSource audi;
+    // Use this for initialization
+    void Start()
+    {
+        audi = GetComponent<AudioSource>();
+        audi.pitch = Random.Range(0.1f, 3f);
+        Destroy(gameObject, 1f);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward);
+    }
+}
---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public GameObject enemy;
+    public Transform[] spawnPoints;
+    // Use this for initialization
+    void Start()
+    {
+        //Spawn our enemies repeatedly
+        InvokeRepeating("SpawnEnemy", 2f, 0.3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SpawnEnemy()
+    {
+        //find a random point between zero and the amount of possible spawn points
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+        //Create our enemy at the randomly selected point
+        Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+
+    }
+}
